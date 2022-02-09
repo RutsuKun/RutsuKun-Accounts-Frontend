8f645f340d3b489386c888f9de778f0dac493c9a
RutsuKun <henicz19@gmail.com>
2022-02-09 01:54:15 +0100

    Change: Prepare frontend for vercel deployments

---
+++ front/src/environments/environment.vercel-dev.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  envName: "DEV",
+  api: "https://dev-rutsukun-accounts-api.henicz.eu",
+  auth: "https://dev-rutsukun-accounts-api.henicz.eu/v1/auth",
+  oauth: "https://dev-rutsukun-accounts-api.henicz.eu/v1/oauth2",
+  front: "https://dev-rutsukun-accounts.henicz.eu",
+
+  admin_portal_scopes: "openid account accounts:read groups:manage admin:clients admin:scopes",
+};
