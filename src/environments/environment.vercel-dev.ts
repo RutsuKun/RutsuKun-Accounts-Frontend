2460f2da254a8bb4aca7e00bf12435bae3510928
RutsuKun <henicz19@gmail.com>
2022-05-06 02:56:01 +0200

    updated: vercel dev build settings

--- src/environments/environment.vercel-dev.ts
+++ src/environments/environment.vercel-dev.ts
@@ -6,5 +6,5 @@ export const environment = {
   oauth: "https://dev-rutsukun-accounts-api.henicz.eu/v1/oauth2",
   front: "https://dev-rutsukun-accounts.henicz.eu",
 
-  admin_portal_scopes: "openid account accounts:read groups:manage admin:clients admin:scopes",
+  admin_portal_scopes: "openid account admin:access",
 };
